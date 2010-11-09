3871ad3091a1e757a2de78b918351302d2a0c7e4
Clinton Sheppard <gar3ts@gmail.com>
2010-11-09 12:57:56 -0600

    let resharper add license header to all .cs files

--- src/FluentAssert.Tests/AssertExtensionsTests.cs
+++ src/FluentAssert.Tests/AssertExtensionsTests.cs
@@ -1,3 +1,13 @@
+//  * **************************************************************************
+//  * Copyright (c) McCreary, Veselka, Bragg & Allen, P.C.
+//  * This source code is subject to terms and conditions of the MIT License.
+//  * A copy of the license can be found in the License.txt file
+//  * at the root of this distribution. 
+//  * By using this source code in any fashion, you are agreeing to be bound by 
+//  * the terms of the MIT License.
+//  * You must not remove this notice from this software.
+//  * **************************************************************************
+
 using System;
 using System.Collections.Generic;
 
--- src/FluentAssert/Exceptions/ShouldBeLessThanOrEqualToAssertionException.cs
+++ src/FluentAssert/Exceptions/ShouldBeLessThanOrEqualToAssertionException.cs
@@ -1,4 +1,13 @@
-﻿namespace FluentAssert.Exceptions
+﻿//  * **************************************************************************
+//  * Copyright (c) McCreary, Veselka, Bragg & Allen, P.C.
+//  * This source code is subject to terms and conditions of the MIT License.
+//  * A copy of the license can be found in the License.txt file
+//  * at the root of this distribution. 
+//  * By using this source code in any fashion, you are agreeing to be bound by 
+//  * the terms of the MIT License.
+//  * You must not remove this notice from this software.
+//  * **************************************************************************
+namespace FluentAssert.Exceptions
 {
 	public class ShouldBeLessThanOrEqualToAssertionException : AssertionException
 	{
