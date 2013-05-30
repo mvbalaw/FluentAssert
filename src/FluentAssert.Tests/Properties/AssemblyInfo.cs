b086aa7daf8f33a4af2fb81b17ea5e8173cf1012
Weston M. Binford III <wbinford@gmail.com>
2013-05-30 16:34:56 -0500

    Added CommonAssemblyInfo.cs to both project files

--- src/FluentAssert.Tests/Properties/AssemblyInfo.cs
+++ src/FluentAssert.Tests/Properties/AssemblyInfo.cs
@@ -15,34 +15,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly:AssemblyTitle("FluentAssert.Tests")]
-[assembly:AssemblyDescription("")]
-[assembly:AssemblyConfiguration("")]
-[assembly:AssemblyCompany("Microsoft")]
-[assembly:AssemblyProduct("FluentAssert.Tests")]
-[assembly:AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly:AssemblyTrademark("")]
-[assembly:AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly:ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly:Guid("32367953-0c52-476c-97f5-4c1e7e39af5e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.0.0")]
